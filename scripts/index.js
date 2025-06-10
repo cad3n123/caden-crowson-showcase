525f801f892c818c0281f60955029cef12ef34b7
cad3n123 <cadencrowson@gmail.com>
2025-06-09 20:13:28 -0500

    First commit

---
+++ scripts/index.js
@@ -0,0 +1,38 @@
+// Elements
+const $iframe = document.querySelector('iframe');
+const $iframeLink = document.querySelector('#video-content > a');
+const [ $select, $videoContent ] = [ 'select', 'video-content' ].map(id => document.getElementById(id));
+
+// Constant Variables
+
+// Event Listeners
+document.addEventListener('DOMContentLoaded', () => {
+    [
+      {
+        url: 'https://RAREROOMEast.com',
+        title: 'RAREROOM'
+      },
+      {
+        url: './',
+        title: 'This Website'
+      }
+    ].forEach(({ url, title }) => {
+        const $vhs = document.createElement('div');
+        $vhs.classList.add('vhs');
+        $vhs.setAttribute('data-url', url);
+        $vhs.innerHTML = title;
+        $vhs.addEventListener('click', mouseEvent => {
+          const URL = mouseEvent.target.getAttribute('data-url');
+          $videoContent.classList.add('active');
+          $iframe.src = URL;
+          $iframeLink.href = URL;
+        });
+
+        $select.appendChild($vhs);
+    });
+
+    setTimeout(() => $videoContent.classList.add('active'), 500);
+});
+$iframe.addEventListener('click', (mouseEvent) => {
+    console.log(mouseEvent.target);
+});
