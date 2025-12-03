bd96bd2b20ba3089eddb3e9561d1ee841b633b61
cad3n123 <cadencrowson@gmail.com>
2025-12-03 15:32:25 -0600

    Added both websites

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,44 @@
 // Elements
 const $iframe = document.querySelector('iframe');
 const $iframeLink = document.querySelector('#video-content > a');
-const [ $select, $videoContent ] = [ 'select', 'video-content' ].map(id => document.getElementById(id));
+const [$select, $videoContent] = ['select', 'video-content'].map((id) =>
+  document.getElementById(id)
+);
 
 // Constant Variables
 
 // Event Listeners
 document.addEventListener('DOMContentLoaded', () => {
-    [
-      {
-        url: './infinite-matter',
-        title: 'Infinite Matter'
-      },
-      {
-        url: './',
-        title: 'This Website'
-      }
-    ].forEach(({ url, title }) => {
-        const $vhs = document.createElement('div');
-        $vhs.classList.add('vhs');
-        $vhs.setAttribute('data-url', url);
-        $vhs.innerHTML = title;
-        $vhs.addEventListener('click', mouseEvent => {
-          const URL = mouseEvent.target.getAttribute('data-url');
-          $videoContent.classList.add('active');
-          $iframe.src = URL;
-          $iframeLink.href = URL;
-        });
-
-        $select.appendChild($vhs);
+  [
+    {
+      url: './infinite-matter',
+      title: 'Infinite Matter',
+    },
+    {
+      url: './infinite-factory',
+      title: 'Infinite Factory',
+    },
+    {
+      url: './',
+      title: 'This Website',
+    },
+  ].forEach(({ url, title }) => {
+    const $vhs = document.createElement('div');
+    $vhs.classList.add('vhs');
+    $vhs.setAttribute('data-url', url);
+    $vhs.innerHTML = title;
+    $vhs.addEventListener('click', (mouseEvent) => {
+      const URL = mouseEvent.target.getAttribute('data-url');
+      $videoContent.classList.add('active');
+      $iframe.src = URL;
+      $iframeLink.href = URL;
     });
 
-    setTimeout(() => $videoContent.classList.add('active'), 500);
+    $select.appendChild($vhs);
+  });
+
+  setTimeout(() => $videoContent.classList.add('active'), 500);
 });
 $iframe.addEventListener('click', (mouseEvent) => {
-    console.log(mouseEvent.target);
+  console.log(mouseEvent.target);
 });
